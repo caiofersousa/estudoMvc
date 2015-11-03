fccb71570b94c3a9c731ec54d4d14ba86e6b3e32
caio <caiofersousa@hotmail.com>
2015-11-03 14:07:05 -0200

    commit inicial

---
+++ src/br/com/turingBits/view/Cliente.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.turingBits.view;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/**
+ *
+ * @author caio
+ */
+@Entity
+@Table(name = "CLIENTE", catalog = "", schema = "APP")
+@NamedQueries({
+    @NamedQuery(name = "Cliente.findAll", query = "SELECT c FROM Cliente c"),
+    @NamedQuery(name = "Cliente.findById", query = "SELECT c FROM Cliente c WHERE c.id = :id"),
+    @NamedQuery(name = "Cliente.findByNome", query = "SELECT c FROM Cliente c WHERE c.nome = :nome"),
+    @NamedQuery(name = "Cliente.findByEndereco", query = "SELECT c FROM Cliente c WHERE c.endereco = :endereco"),
+    @NamedQuery(name = "Cliente.findByCidade", query = "SELECT c FROM Cliente c WHERE c.cidade = :cidade"),
+    @NamedQuery(name = "Cliente.findByEstado", query = "SELECT c FROM Cliente c WHERE c.estado = :estado"),
+    @NamedQuery(name = "Cliente.findByCpf", query = "SELECT c FROM Cliente c WHERE c.cpf = :cpf")})
+public class Cliente implements Serializable {
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Column(name = "NOME")
+    private String nome;
+    @Column(name = "ENDERECO")
+    private String endereco;
+    @Column(name = "CIDADE")
+    private String cidade;
+    @Column(name = "ESTADO")
+    private String estado;
+    @Basic(optional = false)
+    @Column(name = "CPF")
+    private String cpf;
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer id) {
+        this.id = id;
+    }
+
+    public Cliente(Integer id, String cpf) {
+        this.id = id;
+        this.cpf = cpf;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        Integer oldId = this.id;
+        this.id = id;
+        changeSupport.firePropertyChange("id", oldId, id);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        String oldNome = this.nome;
+        this.nome = nome;
+        changeSupport.firePropertyChange("nome", oldNome, nome);
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        String oldEndereco = this.endereco;
+        this.endereco = endereco;
+        changeSupport.firePropertyChange("endereco", oldEndereco, endereco);
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        String oldCidade = this.cidade;
+        this.cidade = cidade;
+        changeSupport.firePropertyChange("cidade", oldCidade, cidade);
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        String oldEstado = this.estado;
+        this.estado = estado;
+        changeSupport.firePropertyChange("estado", oldEstado, estado);
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        String oldCpf = this.cpf;
+        this.cpf = cpf;
+        changeSupport.firePropertyChange("cpf", oldCpf, cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cliente)) {
+            return false;
+        }
+        Cliente other = (Cliente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.turingBits.view.Cliente[ id=" + id + " ]";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+    
+}
---
+++ src/br/com/turingBits/view/Compras.java
@@ -0,0 +1,152 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.turingBits.view;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+/**
+ *
+ * @author caio
+ */
+@Entity
+@Table(name = "COMPRAS", catalog = "", schema = "APP")
+@NamedQueries({
+    @NamedQuery(name = "Compras.findAll", query = "SELECT c FROM Compras c"),
+    @NamedQuery(name = "Compras.findById", query = "SELECT c FROM Compras c WHERE c.id = :id"),
+    @NamedQuery(name = "Compras.findByData", query = "SELECT c FROM Compras c WHERE c.data = :data"),
+    @NamedQuery(name = "Compras.findByDescricao", query = "SELECT c FROM Compras c WHERE c.descricao = :descricao"),
+    @NamedQuery(name = "Compras.findByQtd", query = "SELECT c FROM Compras c WHERE c.qtd = :qtd"),
+    @NamedQuery(name = "Compras.findByPrecouni", query = "SELECT c FROM Compras c WHERE c.precouni = :precouni")})
+public class Compras implements Serializable {
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Column(name = "DATA")
+    @Temporal(TemporalType.DATE)
+    private Date data;
+    @Column(name = "DESCRICAO")
+    private String descricao;
+    @Basic(optional = false)
+    @Column(name = "QTD")
+    private int qtd;
+    @Basic(optional = false)
+    @Column(name = "PRECOUNI")
+    private long precouni;
+
+    public Compras() {
+    }
+
+    public Compras(Integer id) {
+        this.id = id;
+    }
+
+    public Compras(Integer id, int qtd, long precouni) {
+        this.id = id;
+        this.qtd = qtd;
+        this.precouni = precouni;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        Integer oldId = this.id;
+        this.id = id;
+        changeSupport.firePropertyChange("id", oldId, id);
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        Date oldData = this.data;
+        this.data = data;
+        changeSupport.firePropertyChange("data", oldData, data);
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        String oldDescricao = this.descricao;
+        this.descricao = descricao;
+        changeSupport.firePropertyChange("descricao", oldDescricao, descricao);
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(int qtd) {
+        int oldQtd = this.qtd;
+        this.qtd = qtd;
+        changeSupport.firePropertyChange("qtd", oldQtd, qtd);
+    }
+
+    public long getPrecouni() {
+        return precouni;
+    }
+
+    public void setPrecouni(long precouni) {
+        long oldPrecouni = this.precouni;
+        this.precouni = precouni;
+        changeSupport.firePropertyChange("precouni", oldPrecouni, precouni);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Compras)) {
+            return false;
+        }
+        Compras other = (Compras) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.turingBits.view.Compras[ id=" + id + " ]";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+    
+}
